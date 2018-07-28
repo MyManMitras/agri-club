0b3fef27bb363cead51bb9ed7dbe48f4445dad57
prashanth000mj <prashanth000mj@gmail.com>
2018-07-28 22:51:04 +0530

    photos commit

--- src/components/section/photos/photos.js
+++ src/components/section/photos/photos.js
@@ -1,20 +1,7 @@
-import M107 from '../../../assets/members/siddaraju.JPG';
-import M129 from '../../../assets/members/yogesh.JPG';
-import M041 from '../../../assets/members/krishnappaMC.jpg';
-import M085 from '../../../assets/members/RAMESH.jpg';
-import M164 from '../../../assets/members/UMESH.JPG';
-import M040 from '../../../assets/members/KRISHNA.jpg';
-import M028 from '../../../assets/members/GURUBASAPA.JPG';
-import M053 from '../../../assets/members/MAHESHMR.JPG';
-import M049 from '../../../assets/members/MAHALINGA.JPG';
-import M083 from '../../../assets/members/RAKI.JPG';
-import M096 from '../../../assets/members/SATISHHS.jpg';
-import M114 from '../../../assets/members/SUDHIR.JPG';
-import blankPhoto from '../../../assets/blankPhoto.jpg';
-
-
+import blankPhoto from '../../../assets/members/blankPhoto.jpg';
+import M001 from '../../../assets/members/M001.JPG';
 import M002 from '../../../assets/members/M002.JPG';
-
+import M003 from '../../../assets/members/M003.JPG';
 import M004 from '../../../assets/members/M004.jpg';
 import M005 from '../../../assets/members/M005.jpg';
 import M006 from '../../../assets/members/M006.JPG';
@@ -22,100 +9,110 @@ import M007 from '../../../assets/members/M007.JPG';
 import M008 from '../../../assets/members/M008.JPG';
 import M009 from '../../../assets/members/M009.JPG';
 import M010 from '../../../assets/members/M010.jpg';
-
+import M011 from '../../../assets/members/M011.jpg';
 import M012 from '../../../assets/members/M012.JPG';
-
+import M013 from '../../../assets/members/M013.JPG';
 import M014 from '../../../assets/members/M014.jpg';
 import M015 from '../../../assets/members/M015.JPG';
-
-
-
+import M016 from '../../../assets/members/M016.jpg';
+import M017 from '../../../assets/members/M017.JPG';
+import M018 from '../../../assets/members/M018.JPG';
 import M019 from '../../../assets/members/M019.JPG';
 import M020 from '../../../assets/members/M020.JPG';
-
+import M021 from '../../../assets/members/M021.JPG';
 import M022 from '../../../assets/members/M022.JPG';
-
+import M023 from '../../../assets/members/M023.JPG';
 import M024 from '../../../assets/members/M024.JPG';
 import M025 from '../../../assets/members/M025.JPG';
 import M026 from '../../../assets/members/M026.JPG';
 import M027 from '../../../assets/members/M027.jpg';
+import M028 from '../../../assets/members/M028.JPG';
 import M029 from '../../../assets/members/M029.JPG';
 
 import M031 from '../../../assets/members/M031.jpg';
 import M032 from '../../../assets/members/M032.JPG';
-
+import M033 from '../../../assets/members/M033.JPG';
 import M034 from '../../../assets/members/M034.jpg';
 import M035 from '../../../assets/members/M035.JPG';
 import M036 from '../../../assets/members/M036.JPG';
 import M037 from '../../../assets/members/M037.JPG';
 import M038 from '../../../assets/members/M038.JPG';
-
+import M039 from '../../../assets/members/M039.JPG';
+import M040 from '../../../assets/members/M040.jpg';
+import M041 from '../../../assets/members/M041.jpg';
 import M042 from '../../../assets/members/M042.JPG';
 import M043 from '../../../assets/members/M043.JPG';
 import M044 from '../../../assets/members/M044.JPG';
-
+import M045 from '../../../assets/members/M045.JPG';
 import M046 from '../../../assets/members/M046.jpg';
-
+import M047 from '../../../assets/members/M047.jpg';
 import M048 from '../../../assets/members/M048.jpg';
+import M049 from '../../../assets/members/M049.JPG';
 import M050 from '../../../assets/members/M050.JPG';
 import M051 from '../../../assets/members/M051.jpg';
 import M052 from '../../../assets/members/M052.JPG';
+import M053 from '../../../assets/members/M053.JPG';
 import M054 from '../../../assets/members/M054.JPG';
 import M055 from '../../../assets/members/M055.JPG';
 import M056 from '../../../assets/members/M056.jpg';
 import M057 from '../../../assets/members/M057.jpg';
 import M058 from '../../../assets/members/M058.jpg';
-
+import M059 from '../../../assets/members/M059.JPG';
 import M060 from '../../../assets/members/M060.jpg';
 import M061 from '../../../assets/members/M061.jpg';
 import M062 from '../../../assets/members/M062.JPG';
 import M063 from '../../../assets/members/M063.jpg';
-
+import M064 from '../../../assets/members/M064.JPG';
 import M065 from '../../../assets/members/M065.jpg';
 import M066 from '../../../assets/members/M066.jpg';
 import M067 from '../../../assets/members/M067.JPG';
 import M068 from '../../../assets/members/M068.JPG';
-
+import M069 from '../../../assets/members/M069.JPG';
 import M070 from '../../../assets/members/M070.jpg';
-
+import M071 from '../../../assets/members/M071.JPG';
 import M072 from '../../../assets/members/M072.JPG';
 import M073 from '../../../assets/members/M073.JPG';
-
+import M074 from '../../../assets/members/M074.JPG';
 import M075 from '../../../assets/members/M075.JPG';
-
+import M076 from '../../../assets/members/M076.JPG';
 import M077 from '../../../assets/members/M077.jpg';
-
-
+import M078 from '../../../assets/members/M078.JPG';
+import M079 from '../../../assets/members/M079.JPG';
 
 import M081 from '../../../assets/members/M081.JPG';
 import M082 from '../../../assets/members/M082.JPG';
+import M083 from '../../../assets/members/M083.JPG';
 import M084 from '../../../assets/members/M084.JPG';
+import M085 from '../../../assets/members/M085.jpg';
 import M086 from '../../../assets/members/M086.JPG';
 import M087 from '../../../assets/members/M087.JPG';
 import M088 from '../../../assets/members/M088.JPG';
 import M089 from '../../../assets/members/M089.JPG';
-
+import M090 from '../../../assets/members/M090.JPG';
 import M091 from '../../../assets/members/M091.jpg';
 import M092 from '../../../assets/members/M092.JPG';
 import M093 from '../../../assets/members/M093.JPG';
 import M094 from '../../../assets/members/M094.JPG';
+import M095 from '../../../assets/members/M095.JPG';
+import M096 from '../../../assets/members/M096.jpg';
 
-
-
+import M098 from '../../../assets/members/M098.JPG';
 import M099 from '../../../assets/members/M099.JPG';
-
+import M100 from '../../../assets/members/M100.JPG';
 import M101 from '../../../assets/members/M101.JPG';
 import M102 from '../../../assets/members/M102.JPG';
-
+import M103 from '../../../assets/members/M103.JPG';
 import M104 from '../../../assets/members/M104.JPG';
 import M105 from '../../../assets/members/M105.JPG';
 import M106 from '../../../assets/members/M106.JPG';
+import M107 from '../../../assets/members/M107.JPG';
 import M108 from '../../../assets/members/M108.JPG';
 import M109 from '../../../assets/members/M109.JPG';
 import M110 from '../../../assets/members/M110.JPG';
-
+import M111 from '../../../assets/members/M111.JPG';
 import M112 from '../../../assets/members/M112.JPG';
 import M113 from '../../../assets/members/M113.JPG';
+import M114 from '../../../assets/members/M114.JPG';
 import M115 from '../../../assets/members/M115.JPG';
 import M116 from '../../../assets/members/M116.JPG';
 import M117 from '../../../assets/members/M117.JPG';
@@ -123,13 +120,14 @@ import M118 from '../../../assets/members/M118.JPG';
 import M119 from '../../../assets/members/M119.JPG';
 import M120 from '../../../assets/members/M120.JPG';
 import M121 from '../../../assets/members/M121.JPG';
-
+import M122 from '../../../assets/members/M122.JPG';
 import M123 from '../../../assets/members/M123.JPG';
 
 import M125 from '../../../assets/members/M125.JPG';
-
-
+import M126 from '../../../assets/members/M126.JPG';
+import M127 from '../../../assets/members/M127.JPG';
 import M128 from '../../../assets/members/M128.JPG';
+import M129 from '../../../assets/members/M129.JPG';
 import M130 from '../../../assets/members/M130.JPG';
 import M131 from '../../../assets/members/M131.jpg';
 import M132 from '../../../assets/members/M132.JPG';
@@ -138,12 +136,12 @@ import M134 from '../../../assets/members/M134.jpg';
 import M135 from '../../../assets/members/M135.jpg';
 import M136 from '../../../assets/members/M136.JPG';
 import M137 from '../../../assets/members/M137.JPG';
-
+import M138 from '../../../assets/members/M138.JPG';
 import M139 from '../../../assets/members/M139.JPG';
 
 import M141 from '../../../assets/members/M141.JPG';
 import M142 from '../../../assets/members/M142.JPG';
-
+import M143 from '../../../assets/members/M143.JPG';
 import M144 from '../../../assets/members/M144.jpg';
 import M145 from '../../../assets/members/M145.JPG';
 import M146 from '../../../assets/members/M146.jpg';
@@ -154,28 +152,29 @@ import M150 from '../../../assets/members/M150.JPG';
 import M151 from '../../../assets/members/M151.JPG';
 import M152 from '../../../assets/members/M152.jpg';
 import M153 from '../../../assets/members/M153.JPG';
-
+import M154 from '../../../assets/members/M154.JPG';
 import M155 from '../../../assets/members/M155.JPG';
 import M156 from '../../../assets/members/M156.JPG';
 import M157 from '../../../assets/members/M157.JPG';
 import M158 from '../../../assets/members/M158.jpg';
 import M159 from '../../../assets/members/M159.JPG';
 import M160 from '../../../assets/members/M160.JPG';
-
+import M161 from '../../../assets/members/M161.JPG';
 import M162 from '../../../assets/members/M162.JPG';
 import M163 from '../../../assets/members/M163.jpg';
+import M164 from '../../../assets/members/M164.JPG';
 import M165 from '../../../assets/members/M165.JPG';
 import M166 from '../../../assets/members/M166.jpg';
 import M167 from '../../../assets/members/M167.JPG';
-
-
+import M168 from '../../../assets/members/M168.JPG';
+import M169 from '../../../assets/members/M169.JPG';
 import M170 from '../../../assets/members/M170.jpg';
 import M171 from '../../../assets/members/M171.JPG';
 import M172 from '../../../assets/members/M172.jpg';
 import M173 from '../../../assets/members/M173.jpg';
 import M174 from '../../../assets/members/M174.JPG';
 import M175 from '../../../assets/members/M175.JPG';
-
+import M176 from '../../../assets/members/M176.JPG';
 import M177 from '../../../assets/members/M177.JPG';
 import M178 from '../../../assets/members/M178.jpg';
 import M179 from '../../../assets/members/M179.jpg';
@@ -193,37 +192,39 @@ import M190 from '../../../assets/members/M190.JPG';
 import M191 from '../../../assets/members/M191.JPG';
 import M192 from '../../../assets/members/M192.JPG';
 
+import M194 from '../../../assets/members/M194.jpg';
+import RPP from '../../../assets/members/RPP.JPG';
 import SM001 from '../../../assets/members/SM001.jpg';
-
+import SM002 from '../../../assets/members/SM002.JPG';
 
 import SM004 from '../../../assets/members/SM004.JPG';
 import SM005 from '../../../assets/members/SM005.JPG';
-
-
-
-
-
-
+import SM006 from '../../../assets/members/SM006.JPG';
+import SM007 from '../../../assets/members/SM007.jpg';
+import SM008 from '../../../assets/members/SM008.jpg';
+import SM009 from '../../../assets/members/SM009.JPG';
+import SM010 from '../../../assets/members/SM010.JPG';
+import SM011 from '../../../assets/members/SM011.JPG';
 
 import SM013 from '../../../assets/members/SM013.JPG';
-
-
+import SM014 from '../../../assets/members/SM014.JPG';
+import SM015 from '../../../assets/members/SM015.jpg';
 import SM016 from '../../../assets/members/SM016.jpg';
 import SM017 from '../../../assets/members/SM017.JPG';
 import SM018 from '../../../assets/members/SM018.JPG';
-
-
-
+import SM019 from '../../../assets/members/SM019.jpg';
+import SM020 from '../../../assets/members/SM020.JPG';
+import SM021 from '../../../assets/members/SM021.JPG';
 import SM022 from '../../../assets/members/SM022.JPG';
 
-
-
+import SM024 from '../../../assets/members/SM024.JPG';
+import SM025 from '../../../assets/members/SM025.JPG';
 
 import SM027 from '../../../assets/members/SM027.jpg';
 import SM028 from '../../../assets/members/SM028.jpg';
 import SM029 from '../../../assets/members/SM029.jpg';
 
-
+import SM031 from '../../../assets/members/SM031.jpg';
 
 
 
@@ -236,7 +237,7 @@ import SM040 from '../../../assets/members/SM040.jpg';
 import SM041 from '../../../assets/members/SM041.JPG';
 import SM042 from '../../../assets/members/SM042.JPG';
 import SM043 from '../../../assets/members/SM043.jpg';
-
+import SM044 from '../../../assets/members/SM044.JPG';
 import SM045 from '../../../assets/members/SM045.jpg';
 import SM046 from '../../../assets/members/SM046.JPG';
 import SM047 from '../../../assets/members/SM047.jpg';
@@ -244,30 +245,31 @@ import SM047 from '../../../assets/members/SM047.jpg';
 import SM049 from '../../../assets/members/SM049.jpg';
 import SM050 from '../../../assets/members/SM050.JPG';
 import SM051 from '../../../assets/members/SM051.jpg';
+import SM052 from '../../../assets/members/SM052.JPG';
 
+import SM054 from '../../../assets/members/SM054.JPG';
+import SM055 from '../../../assets/members/SM055.JPG';
 
-
-
-
-
-
+import SM057 from '../../../assets/members/SM057.JPG';
+import SM058 from '../../../assets/members/SM058.jpg';
 import SM059 from '../../../assets/members/SM059.JPG';
 import SM060 from '../../../assets/members/SM060.JPG';
 import SM061 from '../../../assets/members/SM061.JPG';
-
-
+import SM062 from '../../../assets/members/SM062.JPG';
+import SM063 from '../../../assets/members/SM063.JPG';
 import SM064 from '../../../assets/members/SM064.jpg';
 import SM065 from '../../../assets/members/SM065.JPG';
 
 import SM067 from '../../../assets/members/SM067.jpg';
 
+import SM069 from '../../../assets/members/SM069.JPG';
+import SM070 from '../../../assets/members/SM070.jpg';
 
 let memberPhotos = {
-
     blankPhoto : blankPhoto,
-    M001 : blankPhoto,
+    M001 : M001,
     M002 : M002,
-    M003 : blankPhoto,
+    M003 : M003,
     M004 : M004,
     M005 : M005,
     M006 : M006,
@@ -275,19 +277,19 @@ let memberPhotos = {
     M008 : M008,
     M009 : M009,
     M010 : M010,
-    M011 : blankPhoto,
+    M011 : M011,
     M012 : M012,
-    M013 : blankPhoto,
+    M013 : M013,
     M014 : M014,
     M015 : M015,
-    M016 : blankPhoto,
-    M017 : blankPhoto,
-    M018 : blankPhoto,
+    M016 : M016,
+    M017 : M017,
+    M018 : M018,
     M019 : M019,
     M020 : M020,
-    M021 : blankPhoto,
+    M021 : M021,
     M022 : M022,
-    M023 : blankPhoto,
+    M023 : M023,
     M024 : M024,
     M025 : M025,
     M026 : M026,
@@ -297,21 +299,21 @@ let memberPhotos = {
     M030 : blankPhoto,
     M031 : M031,
     M032 : M032,
-    M033 : blankPhoto,
+    M033 : M033,
     M034 : M034,
     M035 : M035,
     M036 : M036,
     M037 : M037,
     M038 : M038,
-    M039 : blankPhoto,
+    M039 : M039,
     M040 : M040,
     M041 : M041,
     M042 : M042,
     M043 : M043,
     M044 : M044,
-    M045 : blankPhoto,
+    M045 : M045,
     M046 : M046,
-    M047 : blankPhoto,
+    M047 : M047,
     M048 : M048,
     M049 : M049,
     M050 : M050,
@@ -323,27 +325,27 @@ let memberPhotos = {
     M056 : M056,
     M057 : M057,
     M058 : M058,
-    M059 : blankPhoto,
+    M059 : M059,
     M060 : M060,
     M061 : M061,
     M062 : M062,
     M063 : M063,
-    M064 : blankPhoto,
+    M064 : M064,
     M065 : M065,
     M066 : M066,
     M067 : M067,
     M068 : M068,
-    M069 : blankPhoto,
+    M069 : M069,
     M070 : M070,
-    M071 : blankPhoto,
+    M071 : M071,
     M072 : M072,
     M073 : M073,
-    M074 : blankPhoto,
+    M074 : M074,
     M075 : M075,
-    M076 : blankPhoto,
+    M076 : M076,
     M077 : M077,
-    M078 : blankPhoto,
-    M079 : blankPhoto,
+    M078 : M078,
+    M079 : M079,
     M080 : blankPhoto,
     M081 : M081,
     M082 : M082,
@@ -354,20 +356,20 @@ let memberPhotos = {
     M087 : M087,
     M088 : M088,
     M089 : M089,
-    M090 : blankPhoto,
+    M090 : M090,
     M091 : M091,
     M092 : M092,
     M093 : M093,
     M094 : M094,
-    M095 : blankPhoto,
+    M095 : M095,
     M096 : M096,
     M097 : blankPhoto,
-    M098 : blankPhoto,
+    M098 : M098,
     M099 : M099,
-    M100 : blankPhoto,
+    M100 : M100,
     M101 : M101,
     M102 : M102,
-    M103 : blankPhoto,
+    M103 : M103,
     M104 : M104,
     M105 : M105,
     M106 : M106,
@@ -375,7 +377,7 @@ let memberPhotos = {
     M108 : M108,
     M109 : M109,
     M110 : M110,
-    M111 : blankPhoto,
+    M111 : M111,
     M112 : M112,
     M113 : M113,
     M114 : M114,
@@ -386,12 +388,12 @@ let memberPhotos = {
     M119 : M119,
     M120 : M120,
     M121 : M121,
-    M122 : blankPhoto,
+    M122 : M122,
     M123 : M123,
     M124 : blankPhoto,
     M125 : M125,
-    M126 : blankPhoto,
-    M127 : blankPhoto,
+    M126 : M126,
+    M127 : M127,
     M128 : M128,
     M129 : M129,
     M130 : M130,
@@ -402,12 +404,12 @@ let memberPhotos = {
     M135 : M135,
     M136 : M136,
     M137 : M137,
-    M138 : blankPhoto,
+    M138 : M138,
     M139 : M139,
     M140 : blankPhoto,
     M141 : M141,
     M142 : M142,
-    M143 : blankPhoto,
+    M143 : M143,
     M144 : M144,
     M145 : M145,
     M146 : M146,
@@ -418,29 +420,29 @@ let memberPhotos = {
     M151 : M151,
     M152 : M152,
     M153 : M153,
-    M154 : blankPhoto,
+    M154 : M154,
     M155 : M155,
     M156 : M156,
     M157 : M157,
     M158 : M158,
     M159 : M159,
     M160 : M160,
-    M161 : blankPhoto,
+    M161 : M161,
     M162 : M162,
     M163 : M163,
     M164 : M164,
     M165 : M165,
     M166 : M166,
     M167 : M167,
-    M168 : blankPhoto,
-    M169 : blankPhoto,
+    M168 : M168,
+    M169 : M169,
     M170 : M170,
     M171 : M171,
     M172 : M172,
     M173 : M173,
     M174 : M174,
     M175 : M175,
-    M176 : blankPhoto,
+    M176 : M176,
     M177 : M177,
     M178 : M178,
     M179 : M179,
@@ -457,37 +459,79 @@ let memberPhotos = {
     M190 : M190,
     M191 : M191,
     M192 : M192,
+    M193 : blankPhoto,
+    M194 : M194,
+    RPP : RPP,
     SM001 : SM001,
+    SM002 : SM002,
+    SM003 : blankPhoto,
     SM004 : SM004,
     SM005 : SM005,
+    SM006 : SM006,
+    SM007 : SM007,
+    SM008 : SM008,
+    SM009 : SM009,
+    SM010 : SM010,
+    SM011 : SM011,
+    SM012 : blankPhoto,
     SM013 : SM013,
+    SM014 : SM014,
+    SM015 : SM015,
     SM016 : SM016,
     SM017 : SM017,
     SM018 : SM018,
+    SM019 : SM019,
+    SM020 : SM020,
+    SM021 : SM021,
     SM022 : SM022,
+    SM023 : blankPhoto,
+    SM024 : SM024,
+    SM025 : SM025,
+    SM026 : blankPhoto,
     SM027 : SM027,
     SM028 : SM028,
     SM029 : SM029,
+    SM030 : blankPhoto,
+    SM031 : SM031,
+    SM032 : blankPhoto,
+    SM033 : blankPhoto,
+    SM034 : blankPhoto,
     SM035 : SM035,
     SM036 : SM036,
     SM037 : SM037,
     SM038 : SM038,
+    SM039 : blankPhoto,
     SM040 : SM040,
     SM041 : SM041,
     SM042 : SM042,
     SM043 : SM043,
+    SM044 : SM044,
     SM045 : SM045,
     SM046 : SM046,
     SM047 : SM047,
+    SM048 : blankPhoto,
     SM049 : SM049,
     SM050 : SM050,
     SM051 : SM051,
+    SM052 : SM052,
+    SM053 : blankPhoto,
+    SM054 : SM054,
+    SM055 : SM055,
+    SM056 : blankPhoto,
+    SM057 : SM057,
+    SM058 : SM058,
     SM059 : SM059,
     SM060 : SM060,
     SM061 : SM061,
+    SM062 : SM062,
+    SM063 : SM063,
     SM064 : SM064,
     SM065 : SM065,
-    SM067 : SM067
+    SM066 : blankPhoto,
+    SM067 : SM067,
+    SM068 : blankPhoto,
+    SM069 : SM069,
+    SM070 : SM070    
 };
 
 export default memberPhotos;
